4c8ef6015de55ccc5f2c63ad72e1d9fed09444b8
Runnan Li <runnanl@blackmagicdesign.com>
2021-10-14 18:06:04 +1100

    time tool added

---
+++ pak/time.py
@@ -0,0 +1,41 @@
+"""
+Time related on stop functions
+"""
+import time
+from datetime import datetime
+
+
+def datetimestamp(no_microseconds=False):
+    """
+    returns a timestamp of current local date time. Default will give a precision upto microseconds.
+    Format as Date/Month/Year-Hour:Minute:Second.Microsecond
+    """
+    if no_microseconds:
+        return datetime.now().strftime('%m/%d/%Y-%H:%M:%S')
+    else:
+        return datetime.now().strftime('%m/%d/%Y-%H:%M:%S.%f')
+
+
+def timestamp():
+    """
+    returns a pure time stamp precision up to microseconds
+    """
+    return datetime.now().strftime('%H:%M:%S.%f')
+
+
+def stopwatch(func):
+    """
+    A decorator (use before func @stopwatch) takes how long that takes a function to run.
+    :param func: func obj without init
+    """
+    def inner(*args, **kwargs):
+        begin = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        try:
+            f_name = func.__name__
+        except:
+            f_name = ''
+        print(f'Lap time of function {f_name}: {end - begin}s')
+
+    return inner
