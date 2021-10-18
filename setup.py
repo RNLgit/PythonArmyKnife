0a9317b08f369627c2777e1fd548773c6db60fe6
Runnan Li <runnanl@blackmagicdesign.com>
2021-10-18 16:24:25 +1100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="PythonArmyKnife",
+    version="0.0.1",
+    description='A light weight Python utility tool work like an army knife',
+    long_description=open('README.md').read(),
+    classifiers=[],
+    install_requires=['numpy', 'matplotlib', 'pandas', 'opencv-python'],
+    setup_requires=[],
+    scripts=[],
+    python_requires='>=3.6',
+    entry_points={
+    },
+    zip_safe=False,
+    include_package_data=True
+)
